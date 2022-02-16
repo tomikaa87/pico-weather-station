0583d28f908adc9a5317c66bc744af488da9c93d
tomikaa87 <tomikaa87@gmail.com>
2022-02-16 17:50:15 +0100

    Add FMT lib, extract graphics

---
+++ FrontEnd/src/Graphics.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Graphics
+{
+    constexpr auto MainScreen_width = 240;
+    constexpr auto MainScreen_height = 160;
+    extern const unsigned char MainScreen_bits[];
+
+    constexpr auto MainScreenInv_width = 240;
+    constexpr auto MainScreenInv_height = 160;
+    extern const unsigned char MainScreenInv_bits[];
+}
