cc955761c988045714fa577f94f65f0400b932be
tomikaa87 <tomikaa87@gmail.com>
2022-02-16 16:42:47 +0100

    Extract fonts from main.cpp

---
+++ FrontEnd/src/Fonts.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Fonts
+{
+    /*
+        Fontname: Bit Cell 25
+        Copyright:
+        Glyphs: 95/95
+        BBX Build Mode: 0
+    */
+    extern const uint8_t BitCell25[1392];
+
+    /*
+        Fontname: PFT7Condensed
+        Copyright:
+        Glyphs: 95/95
+        BBX Build Mode: 0
+    */
+    extern const uint8_t PFT7Condensed[939];
+}
