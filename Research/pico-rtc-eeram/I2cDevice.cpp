2b7d28c194cf8cbd4014bdb303f1e15d7fa293c4
tomikaa87 <tomikaa87@gmail.com>
2022-10-28 12:42:52 +0200

    Add RTC/EERAM test program

---
+++ Research/pico-rtc-eeram/I2cDevice.cpp
@@ -0,0 +1,170 @@
+#include "I2cDevice.h"
+
+#include <hardware/i2c.h>
+
+#include <cstdio>
+
+#define DEBUG_I2C_FUNCTIONS 1
+
+namespace Hardware
+{
+    I2cDevice::I2cDevice(
+        const uint sdaPin,
+        const uint sclPin,
+        const int clockFreq
+    )
+    {
+        gpio_set_function(sdaPin, GPIO_FUNC_I2C);
+        gpio_set_function(sclPin, GPIO_FUNC_I2C);
+
+        const auto baudrate = i2c_init(i2c0, 100 * 1000);
+        printf("I2cDevice: baudrate: %u\r\n", baudrate);
+    }
+
+    bool I2cDevice::startTransmission(const uint8_t address)
+    {
+        printf("I2cDevice::startTransmission: address=%u\r\n", address);
+
+        // Another transmission is already in progress
+        if (address != _currentAddress && _currentAddress != 0) {
+            printf("I2cDevice::startTransmission: another transmission is in progress\r\n`");
+            return false;
+        }
+
+        if (_currentAddress == 0) {
+            i2c0->hw->enable = 0;
+            i2c0->hw->tar = address;
+            i2c0->hw->enable = 1;
+            i2c0->restart_on_next = false;
+        } else {
+            // Repeated start
+            i2c0->restart_on_next = true;
+        }
+
+        _currentAddress = address;
+
+        return true;
+    }
+
+    bool I2cDevice::endTransmission()
+    {
+        printf("I2cDevice::endTransmission: address=%u\r\n", _currentAddress);
+
+        if (_currentAddress == 0) {
+            printf("I2cDevice::endTransmission: no transmission is in progress\r\n");
+            return false;
+        }
+
+        _currentAddress = 0;
+
+        return true;
+    }
+
+    bool I2cDevice::write(
+        const uint8_t* const data,
+        const size_t length,
+        [[maybe_unused]] const bool noStop
+    ) {
+        printf("I2cDevice::write: address=%u, length=%u\r\n", _currentAddress, length);
+
+#if DEBUG_I2C_FUNCTIONS
+        printf(
+            "I2cDevice::write: data=%p, length=%u, noStop=%u\r\n",
+            data,
+            length,
+            noStop
+        );
+
+        printf("I2cDevice::write:");
+#endif
+
+        for (auto i = 0u; i < length; ++i) {
+            bool first = i == 0;
+            bool last = i == length - 1;
+
+#if DEBUG_I2C_FUNCTIONS
+            printf(" %02x", data[i]);
+#endif
+
+            i2c0->hw->data_cmd =
+                bool_to_bit(first && i2c0->restart_on_next) << I2C_IC_DATA_CMD_RESTART_LSB |
+                bool_to_bit(last && !noStop) << I2C_IC_DATA_CMD_STOP_LSB |
+                data[i];
+
+            do {
+                tight_loop_contents();
+            } while (!(i2c0->hw->raw_intr_stat & I2C_IC_RAW_INTR_STAT_TX_EMPTY_BITS));
+        }
+
+#if DEBUG_I2C_FUNCTIONS
+        printf("\r\nI2cDevice::write: finished\r\n");
+#endif
+
+        i2c0->restart_on_next = false;
+
+        return true;
+    }
+
+    bool I2cDevice::read(
+        uint8_t* const data,
+        const size_t length,
+        [[maybe_unused]] const bool noStop
+    ) {
+        printf("I2cDevice::read: address=%u, length=%u\r\n", _currentAddress, length);
+
+        if (_currentAddress == 0) {
+            printf("I2cDevice::read: no transmission is in progress");
+            return false;
+        }
+
+#if DEBUG_I2C_FUNCTIONS
+        printf(
+            "I2cDevice::read: data=%p, length=%u, noStop=%u\r\n",
+            data,
+            length,
+            noStop
+        );
+#endif
+
+        auto abort = false;
+
+#if DEBUG_I2C_FUNCTIONS
+        printf("I2cDevice::read:");
+#endif
+
+        for (auto i = 0u; i < length; ++i) {
+            bool first = i == 0;
+            bool last = i == length - 1;
+
+            while (!i2c_get_write_available(i2c0))
+                tight_loop_contents();
+
+            i2c0->hw->data_cmd =
+                bool_to_bit(first && i2c0->restart_on_next) << I2C_IC_DATA_CMD_RESTART_LSB |
+                bool_to_bit(last && !noStop) << I2C_IC_DATA_CMD_STOP_LSB |
+                I2C_IC_DATA_CMD_CMD_BITS;
+
+            do {
+                abort = static_cast<bool>(i2c0->hw->clr_tx_abrt);
+            } while (!abort && !i2c_get_read_available(i2c0));
+
+            if (abort)
+                break;
+
+            data[i] = static_cast<uint8_t>(i2c0->hw->data_cmd);
+
+#if DEBUG_I2C_FUNCTIONS
+            printf(" %02x", data[i]);
+#endif
+        }
+
+#if DEBUG_I2C_FUNCTIONS
+        printf("\r\nI2cDevice::read: finished\r\n");
+#endif
+
+        i2c0->restart_on_next = false;
+
+        return !abort;
+    }
+
+}
---
+++ Research/pico-rtc-eeram/main.cpp
@@ -0,0 +1,292 @@
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
+#include "hardware/uart.h"
+#include "pico/stdio.h"
+
+#include <cstdio>
+
+#include "I2cDevice.h"
+#include "EERAM_47xxx.h"
+#include "RTC_MCP7940N.h"
+
+namespace Pins::Uart
+{
+    constexpr auto TX = 12;
+    constexpr auto RX = 13;
+}
+
+namespace Pins::I2C
+{
+    constexpr auto SDA = 16;
+    constexpr auto SCL = 17;
+}
+
+namespace Pins::RTC
+{
+    constexpr auto MFP = 18;
+}
+
+namespace I2cFunctions
+{
+    bool startTransmission(void* const arg, const uint8_t address)
+    {
+        auto* const i2c = reinterpret_cast<Hardware::I2cDevice*>(arg);
+        return i2c->startTransmission(address);
+    }
+
+    bool endTransmission(void* const arg)
+    {
+        auto* const i2c = reinterpret_cast<Hardware::I2cDevice*>(arg);
+        return i2c->endTransmission();
+    }
+
+    bool write(
+        void* const arg,
+        const uint8_t* const data,
+        const size_t length,
+        const bool noStop
+    )
+    {
+        auto* const i2c = reinterpret_cast<Hardware::I2cDevice*>(arg);
+        return i2c->write(data, length, noStop);
+    }
+
+    bool read(
+        void* const arg,
+        uint8_t* const data,
+        const size_t length,
+        const bool noStop
+    )
+    {
+        auto* const i2c = reinterpret_cast<Hardware::I2cDevice*>(arg);
+        return i2c->read(data, length, noStop);
+    }
+}
+
+RTC_MCP7940N_Device setupRtc(Hardware::I2cDevice& i2c)
+{
+    gpio_init(Pins::RTC::MFP);
+    gpio_set_dir(Pins::RTC::MFP, false);
+    // MFP is open-drain and requires a pull-up to VCC
+    gpio_set_pulls(Pins::RTC::MFP, true, false);
+
+    RTC_MCP7940N_Device device;
+    device.i2cFunctionArg = &i2c;
+    device.i2cStartTransmission = I2cFunctions::startTransmission; 
+    device.i2cEndTransmission = I2cFunctions::endTransmission;
+    device.i2cRead = I2cFunctions::read;
+    device.i2cWrite = I2cFunctions::write;
+
+    return device;
+}
+
+EERAM_47xxx_Device setupEeram(Hardware::I2cDevice& i2c)
+{
+    EERAM_47xxx_Device device;
+    device.a1 = 0;
+    device.a2 = 0;
+    device.i2cFunctionArg = &i2c;
+    device.i2cStartTransmission = I2cFunctions::startTransmission; 
+    device.i2cEndTransmission = I2cFunctions::endTransmission;
+    device.i2cRead = I2cFunctions::read;
+    device.i2cWrite = I2cFunctions::write;
+
+    return device;
+}
+
+int main()
+{
+    gpio_set_function(Pins::Uart::RX, GPIO_FUNC_UART);
+    gpio_set_function(Pins::Uart::TX, GPIO_FUNC_UART);
+
+    stdio_init_all();
+
+    printf("***\r\nPico RTC MCP7940N and EERAM 47xxx test program\r\n***\r\n");
+
+    Hardware::I2cDevice i2c{ Pins::I2C::SDA, Pins::I2C::SCL };
+
+    const auto rtc = setupRtc(i2c);
+    // const auto eeram = setupEeram(i2c);
+
+    printf("Testing date-time writing...\r\n");
+
+    if (RTC_MCP7940N_SetDateTime(&rtc, 22, 10, 17, 0, 20, 14, 20, false, false)) {
+        printf("Date-time set successfully\r\n");
+    } else {
+        printf("Failed to set date-time\r\n");
+    }
+
+    printf("Testing date-time reading...\r\n");
+
+    uint8_t year = 0;
+    uint8_t month = 0;
+    uint8_t date = 0;
+    uint8_t wd = 0;
+    uint8_t hour = 0;
+    uint8_t minute = 0;
+    uint8_t second = 0;
+    bool mode12h = false;
+    bool pm = false;
+
+    const auto ok = RTC_MCP7940N_GetDateTime(&rtc, &year, &month, &date, &wd, &hour, &minute, &second, &mode12h, &pm);
+
+    printf(
+        "Read finished: ok=%u, y=%u, mo=%u, d=%u, wd=%u, h=%u, m=%u, s=%u, 12h=%u, pm=%u\r\n",
+        ok, year, month, date, wd, hour, minute, second, mode12h, pm
+    );
+
+    printf("Testing SRAM reading/writing...\r\n");
+
+    const char testData[] = "RTC_MCP7940N_WriteSRAM";
+
+    if (
+        RTC_MCP7940N_WriteSRAM(
+            &rtc,
+            0,
+            reinterpret_cast<const uint8_t*>(testData),
+            sizeof(testData)
+        )
+    ) {
+        printf("SRAM write succeeded, reading...\r\n");
+
+        char buffer[64] = { 0 };
+
+        if (
+            RTC_MCP7940N_ReadSRAM(
+                &rtc,
+                0,
+                reinterpret_cast<uint8_t*>(buffer),
+                sizeof(testData)
+            )
+        ) {
+            printf("SRAM read succeeded, data: %s\r\n", buffer);
+        } else {
+            printf("SRAM read failed\r\n");
+        }
+    } else {
+        printf("SRAM write failed\r\n");
+    }
+
+    printf("Setting up Alarm 0...\r\n");
+
+    if (
+        !RTC_MCP7940N_SetAlarm(
+            &rtc,
+            RTC_MCP7940N_ALARM_0,
+            RTC_MCP7940N_ALARM_MASK_SECONDS,
+            RTC_MCP7940N_ALARM_POLARITY_HIGH,
+            0, 0, 0, 0, 0, 0,
+            false, false
+        )
+    ) {
+        printf("Failed to setup Alarm 0\r\n");
+    }
+
+    if (!RTC_MCP7940N_SetAlarmEnabled(&rtc, RTC_MCP7940N_ALARM_0, true)) {
+        printf("Failed to enable Alarm 0\r\n");
+    }
+
+    printf("Setting up Alarm 1...\r\n");
+
+    if (
+        !RTC_MCP7940N_SetAlarm(
+            &rtc,
+            RTC_MCP7940N_ALARM_1,
+            RTC_MCP7940N_ALARM_MASK_SECONDS,
+            RTC_MCP7940N_ALARM_POLARITY_LOW,
+            0, 0, 0, 0, 0, 30,
+            false, false
+        )
+    ) {
+        printf("Failed to setup Alarm 0\r\n");
+    }
+
+    if (!RTC_MCP7940N_SetAlarmEnabled(&rtc, RTC_MCP7940N_ALARM_1, true)) {
+        printf("Failed to enable Alarm 1\r\n");
+    }
+
+    static const char* Weekdays[7] = {
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    };
+
+    while (true) {
+        sleep_ms(1000);
+
+        if (RTC_MCP7940N_GetDateTime(&rtc, &year, &month, &date, &wd, &hour, &minute, &second, &mode12h, &pm)) {
+            printf(
+                "RTC time: %s, 20%02u-%02u-%02u %02u:%02u:%02u\r\n",
+                Weekdays[wd % 7], year, month, date, hour, minute, second
+            );
+        } else {
+            printf("Failed to read RTC date-time\r\n");
+        }
+
+        if (
+            RTC_MCP7940N_GetPowerFailTimeStamp(
+                &rtc,
+                RTC_MCP7940N_POWER_DOWN_TIMESTAMP,
+                &minute, &hour, &date, &wd, &month, &mode12h, &pm
+            )
+        ) {
+            printf(
+                "RTC power-down time-stamp: %s, %02u-%02u %02u:%02u%s\r\n",
+                Weekdays[wd % 7], month, date, hour, minute,
+                mode12h ? (pm ? " PM" : " AM") : ""
+            );
+        } else {
+            printf("Failed to read RTC power-down time-stamp\r\n");
+        }
+
+        if (
+            RTC_MCP7940N_GetPowerFailTimeStamp(
+                &rtc,
+                RTC_MCP7940N_POWER_UP_TIMESTAMP,
+                &minute, &hour, &date, &wd, &month, &mode12h, &pm
+            )
+        ) {
+            printf(
+                "RTC power-up time-stamp: %s, %02u-%02u %02u:%02u%s\r\n",
+                Weekdays[wd % 7], month, date, hour, minute,
+                mode12h ? (pm ? " PM" : " AM") : ""
+            );
+        } else {
+            printf("Failed to read RTC power-up time-stamp\r\n");
+        }
+
+        printf("MFP pin: %u\r\n", gpio_get(Pins::RTC::MFP));
+
+        bool alarm = false;
+        if (RTC_MCP7940N_GetAlarmInterruptFlag(&rtc, RTC_MCP7940N_ALARM_0, &alarm)) {
+            if (alarm) {
+                printf("Alarm 0 triggered\r\n");
+
+                if (!RTC_MCP7940N_ClearAlarmInterruptFlag(&rtc, RTC_MCP7940N_ALARM_0)) {
+                    printf("Failed to clear Alarm 0 IF\r\n");
+                }
+            }
+        } else {
+            printf("Failed to read Alarm 0 IF\r\n");
+        }
+
+        alarm = false;
+        if (RTC_MCP7940N_GetAlarmInterruptFlag(&rtc, RTC_MCP7940N_ALARM_1, &alarm)) {
+            if (alarm) {
+                printf("Alarm 1 triggered\r\n");
+
+                if (!RTC_MCP7940N_ClearAlarmInterruptFlag(&rtc, RTC_MCP7940N_ALARM_1)) {
+                    printf("Failed to clear Alarm 1 IF\r\n");
+                }
+            }
+        } else {
+            printf("Failed to read Alarm 1 IF\r\n");
+        }
+    }
+
+    return 0;
+}
